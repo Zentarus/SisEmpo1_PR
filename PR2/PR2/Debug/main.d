6dafb8aae87158107a7ac7aad471cffe4626fe3e
Zentarus <146416683+Zentarus@users.noreply.github.com>
2025-04-03 13:52:29 +0200

    Add files via upload

---
+++ PR2/PR2/Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdlib.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h
+main.obj: D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/InitSystem.h
+main.obj: D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/clock.h
+main.obj: D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/display.h
+
+../main.c:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdlib.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h:
+
+D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/InitSystem.h:
+
+D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/clock.h:
+
+D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/display.h:
+
---
+++ PR2/PR2/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lfrwp_init.a -l"D:/temporal/SisEmpo1_PR/PR2/Modulos_PR/Debug/Modulos_PR.lib" -llibc.a
+
